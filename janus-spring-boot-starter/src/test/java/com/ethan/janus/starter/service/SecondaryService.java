61e6dfc52b5abd2bf2b0dfab0dd644ff2a46c83d
别小楼 <45735033+sxd19911008@users.noreply.github.com>
2026-02-28 14:10:18 +0800

    optimize: 清理无用代码

--- janus-spring-boot-starter/src/test/java/com/ethan/janus/starter/service/SecondaryService.java
+++ janus-spring-boot-starter/src/test/java/com/ethan/janus/starter/service/SecondaryService.java
@@ -50,7 +50,6 @@ public class SecondaryService implements TestInterface {
         } else if ("err".equals(key)) {
             testRollbackMapper.deleteByKey("pre");
         }
-//        testRollbackMapper.selectNumByKey("exist");
         return new TestResponse(0);
     }
 }
